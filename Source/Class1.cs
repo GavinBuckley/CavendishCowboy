b9ad06d22957ff7d575c8f7a81e851d7e815c6f0
Gavin Buckley <31485432+GavinBuckley@users.noreply.github.com>
2024-05-28 20:08:34 -0500

    Add files via upload

---
+++ Source/Class1.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace CavendishCowboy
+{
+    [BepInPlugin("litttlehawk.CavendishCowboy", "CavendishCowboy", "1.0.0")]
+    public class SpeedrunReset : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            Logger.LogInfo("CavendishCowboy is loaded!");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                CallResetTimer();
+                ResetLevel();
+                Logger.LogInfo("Reset Succsessfully!");
+            }
+        }
+
+        private void CallResetTimer()
+        {
+            try
+            {
+                SpeedRunTimer.ResetTimer();
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError($"Failed to call SpeedRunTimer.ResetTimer(): {ex}");
+            }
+        }
+
+        private void ResetLevel()
+        {
+            try
+            {
+                LevelSwitch.ChangeScene("Tutorial Level");
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError($"Failed to call LevelManager.SelectLevel(): {ex}");
+            }
+        }
+    }
+}
